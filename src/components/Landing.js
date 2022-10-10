19950a9f777c85c91923d6f7c7bbfc9a2988de43
Eriche Gonzales <erichelouise@gmail.com>
2022-10-10 15:12:35 -0400

    arrow animation, learn more

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,16 @@
 import { Container, Image, Row, Col } from "react-bootstrap";
+import { useRef } from "react";
 // import ScrollTrigger from "react-scroll-trigger";
 import eg from "../images/eg.jpg";
 import arrow from "../images/arrow.png";
 
 const Landing = () => {
+   const div2ref = useRef();
+
+  const handleClick = () => {
+    div2ref.current.scrollIntoView({ behavior: "smooth" });
+  }
+
   return (
     <div id="wrapper">
       <div className="div1">
@@ -22,11 +29,16 @@ const Landing = () => {
               <Image id="img-me" roundedCircle src={eg} fluid />
             </Col>
             <Col></Col>
-        <Image className="arrow-img bounce" src={arrow} />
+            <h7 id='learn-more'>Learn more</h7>
+            <Image
+              className="arrow-img bounce"
+              src={arrow}
+              onClick={handleClick}
+            />
           </Row>
         </Container>
       </div>
-      <div className="div2">
+      <div className="div2" ref={div2ref}>
         <Container>
           <Row>
             <Col></Col>
