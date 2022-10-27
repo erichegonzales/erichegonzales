fe8882d1228eb2bc9692341e15829919239eefd1
Eriche Gonzales <erichelouise@gmail.com>
2022-10-26 20:57:06 -0400

    changes images to url sources

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,30 @@
-import { Container, Image, Row, Col, Card, Button } from "react-bootstrap";
-import eg from "../images/EG.JPG";
-import arrow from "../images/arrow.png";
-import project from "../images/project.jpg";
-import contact from "../images/contact.jpg";
+import {
+  Container,
+  Image,
+  Row,
+  Col,
+  Card,
+  Button,
+  CardGroup,
+  Stack,
+} from "react-bootstrap";
+// import eg from "../images/EG.JPG";
+// import arrow from "../images/arrow.png";
+// import project from "../images/project.jpg";
+// import contact from "../images/contact.jpg";
 import { useRef } from "react";
 import Transitions from "./Transitions";
 import Footer from "./Footer";
 
 const Home = () => {
+  const eg = "https://drive.google.com/uc?id=1-FWmwKQU4re7Gp7AevWBUy7jj5i-fBD6";
+
+  const project =
+    "https://drive.google.com/uc?id=1hgR6j-m_fLc-BJICISSLXgZzE74CYWM4";
+
+  const contact =
+    "https://drive.google.com/uc?id=1ZrR64TPl3fHYPBH1V6ql13BjFEYDcHnY";
+
   const div2ref = useRef();
   const div3ref = useRef();
   const div4ref = useRef();
@@ -39,31 +56,57 @@ const Home = () => {
       <div id="wrapper">
         <div className="div1">
           <Container fluid>
-            <Row id="home-col1"></Row>
             <Row>
               <Col></Col>
-              <Col xs={5}>
+            </Row>
+            <Row>
+              <Col></Col>
+              <Col lg={5}>
                 <div id="intro">
                   <h1 id="hello">Hello,</h1>
                   <h1 id="eriche">I'm Eriche</h1>
                   <h4 id="engineer">A software engineer</h4>
+                  <br />
                 </div>
               </Col>
-              <Col xs={5}>
-                <Image id="img-me" roundedCircle src={eg} fluid />
+              <Col lg={5}>
+                <div id="eg-img-div">
+                  <Image id="eg-img" roundedCircle src={eg} fluid />
+                </div>
               </Col>
               <Col></Col>
-              <p id="learn-more">Read more</p>
+
+              {/* <p id="read-more">Read more</p>
+
               <Image
-                className="arrow-img1 bounce"
+                className="arrow img1 bounce"
                 src={arrow}
                 onClick={handleDiv1Click}
-              />
+              /> */}
             </Row>
           </Container>
         </div>
         <div className="div2" ref={div2ref}>
-          <Container>
+          <Container className="home-card-div" fluid>
+            <CardGroup>
+              <Card className="home-card">
+                <Card.Body>
+                  <Card.Title>Who I Am</Card.Title>
+                  <Card.Text>
+                    I am a software engineer experienced in creating full-stack
+                    applications. My skills include, but are not limited to,
+                    Ruby, Rails, JavaScript, React, Python, Django, Java, HTML,
+                    CSS, Git, PostgreSQL, and MySQL. I graduated from Flatiron
+                    School bootcamp in October 2022 where I learned JavaScript
+                    with React and Ruby on Rails. I'm pursuing full-stack or
+                    back-end development roles to begin my career in the tech
+                    industry.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Container>
+          {/* <Container fluid>
             <Row>
               <Col>
                 <div className="left-div who">
@@ -88,11 +131,30 @@ const Home = () => {
                 onClick={handleDiv2Click}
               />
             </Row>
-            {/* <p className="img-cred">Photo by Christopher Gower on Unsplash</p> */}
-          </Container>
+          </Container> */}
         </div>
         <div className="div3" ref={div3ref}>
-          <Container>
+          <Container className="home-card-div" fluid>
+            <CardGroup>
+              <Card className="home-card">
+                <Card.Body>
+                  <Card.Title>What I Bring</Card.Title>
+                  <Card.Text>
+                    I bring my passion to learn, drive to grow, flexibility to
+                    change, and ability to solve problems from a unique
+                    perspective. I bring my qualities as an open-minded
+                    individual, an explorer of new knowledge and skills, and a
+                    person with a strong and resilient mindset. I bring my
+                    experiences as a team leader and collaborator who asks for
+                    help when needed and helps others. That's what I contribute,
+                    along with a bright smile, a bubbly personality, and a
+                    pleasure to work with.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Container>
+          {/* <Container>
             <Row>
               <Col></Col>
               <Col></Col>
@@ -118,11 +180,30 @@ const Home = () => {
                 onClick={handleDiv3Click}
               />
             </Row>
-            {/* <p className="img-cred">Photo by Emiliano Bar on Unsplash</p> */}
-          </Container>
+          </Container> */}
         </div>
         <div className="div4" ref={div4ref}>
-          <Container>
+          <Container className="home-card-div" fluid>
+            <CardGroup>
+              <Card className="home-card">
+                <Card.Body>
+                  <Card.Title>Values {"& "} Interests</Card.Title>
+                  <Card.Text>
+                    I aim to have positive impacts and improve the lives of
+                    others. I enjoy creating and building things. I care about
+                    diversity, inclusivity, equity, and bringing awareness. I
+                    look for growth mindsets and genuine connections. I value a
+                    team with initiative, and a leader that effectively guides
+                    and communicates with their team. I'm interested in being
+                    part of a committed group with hard-working people that
+                    values personal well-being, responsibility, and
+                    accountability.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Container>
+          {/* <Container>
             <Row>
               <Col>
                 <div className="left-div values">
@@ -149,10 +230,29 @@ const Home = () => {
               />
             </Row>
             <p className="img-cred">Photo by Karl Rabe on Flickr</p>
-          </Container>
+          </Container> */}
         </div>
         <div className="div5" ref={div5ref}>
-          <Container>
+          <Container className="home-card-div" fluid>
+            <CardGroup>
+              <Card className="home-card">
+                <Card.Body>
+                  <Card.Title>My Background</Card.Title>
+                  <Card.Text>
+                    I graduated from Vassar College in May 2022 with a
+                    Bachelor's degree, double-majoring in Physics and Astronomy.
+                    I studied topics such as Astrophysics, Quantum Mechanics,
+                    Thermodynamics, Solid State Physics, and more. I learned
+                    Java OOP in a Data Structures {"& "} Algorithms course, and
+                    I completed my senior thesis using Python. I have taken
+                    Calculus I/II, Multivariable Calculus, Linear Algebra,
+                    Applied Math, and Statistics.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </CardGroup>
+          </Container>
+          {/* <Container>
             <Row>
               <Col></Col>
               <Col></Col>
@@ -177,24 +277,17 @@ const Home = () => {
                 onClick={handleDiv5Click}
               />
             </Row>
-            {/* <p className="img-cred">Photo by Rafael Cerqueira on Unsplash</p> */}
-          </Container>
+          </Container> */}
         </div>
         <div className="div6" ref={div6ref}>
-          <Container>
-            <h1 id="interested">Want to learn more?</h1>
-
-            <div className="div6-content">
-              <Row>
-                <Col>
-                  <Card style={{ width: "35rem" }}>
-                    <Card.Img variant="top" src={project} />
-                    {/* <Card.ImgOverlay>
-                    <p className="img-cred card-cred">
-                      Photo by Markus Winkler on Unsplash
-                    </p>
-                  </Card.ImgOverlay> */}
+          <h1 id="check-out">Check out my other pages</h1>
+          <div className="div6-content">
+            <Container>
+              <CardGroup>
+                <Row md={2} lg={2} xl={2}>
+                  <Card className="div6-card">
                     <Card.Body>
+                      <Card.Img variant="top" src={project} />
                       <hr />
                       <Card.Text>Browse the projects I've worked on</Card.Text>
                       <Card.Link href="/projects">
@@ -202,16 +295,10 @@ const Home = () => {
                       </Card.Link>
                     </Card.Body>
                   </Card>
-                </Col>
-                <Col>
-                  <Card style={{ width: "35rem" }}>
-                    <Card.Img variant="top" src={contact} />
-                    {/* <Card.ImgOverlay>
-                    <p className="img-cred card-cred">
-                      Photo by Pawel Czerwinski on Unsplash
-                    </p>
-                  </Card.ImgOverlay> */}
+
+                  <Card className="div6-card">
                     <Card.Body>
+                      <Card.Img variant="top" src={contact} />
                       <hr />
                       <Card.Text>Find me online or send me a message</Card.Text>
                       <Card.Link href="/contact">
@@ -219,10 +306,10 @@ const Home = () => {
                       </Card.Link>
                     </Card.Body>
                   </Card>
-                </Col>
-              </Row>
-            </div>
-          </Container>
+                </Row>
+              </CardGroup>
+            </Container>
+          </div>
         </div>
         <Footer />
       </div>
